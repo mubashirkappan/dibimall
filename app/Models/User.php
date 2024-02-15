cdbf0916d8695281359f18a4f748cb906e107ac4
abhishekiocod <abhishekmenon767@gmail.com>
2024-02-15 22:46:23 +0530

    Filament Updated Version

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,6 +7,8 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
 
 class User extends Authenticatable
 {
@@ -17,6 +19,10 @@ class User extends Authenticatable
      *
      * @var array<int, string>
      */
+    public function canAccessPanel(Panel $panel): bool
+    {
+        return str_ends_with($this->email, '@localhost') && $this->hasVerifiedEmail();
+    }
     protected $fillable = [
         'name',
         'email',
