dea6da8a79546f37585429ac86d60f07e44a7e5e
mubashir kappan <mubashirkappan@gmail.com>
2024-04-28 23:25:30 +0530

    get user api updates

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,12 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\UserResource;
 use Illuminate\Http\Request;
 
-class UserController extends Controller
+class UserController extends BaseController
 {
     public function getUser(){
         $user = auth()->user();
-        return UserResource::collection($user);
+        $data = new UserResource($user);
+        return $this->sendSuccess($data,'user details');
     } 
 }
