be81d2a8a73d92eb45536a8ee55198c6eec16bb9
mubashirkappan <mubashirkappan@gmail.com>
2026-02-13 11:08:40 +0530

    shop order list orderby changed and filament updates

--- app/Filament/Admin/Resources/TasOrderResource/Pages/ListTasOrders.php
+++ app/Filament/Admin/Resources/TasOrderResource/Pages/ListTasOrders.php
@@ -13,7 +13,7 @@ class ListTasOrders extends ListRecords
     protected function getHeaderActions(): array
     {
         return [
-            Actions\CreateAction::make(),
+            // Actions\CreateAction::make(),
         ];
     }
 }
